bc2bb96bdfa3220a195fb23e9838af47b42b0ecf
adeolammy <adeolammy2020@gmail.com>
2023-02-20 14:46:51 +0100

    This is a new commit for what I originally planned to be amended

---
+++ BackEnd/schemas/postedBy.js
@@ -0,0 +1,13 @@
+export default {
+    
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'reference',
+            to: [
+                {
+
+                    type: 'user',
+                },
+                
+            ]     
+}   
