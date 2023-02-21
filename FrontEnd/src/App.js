ded19b7b35ff1c16a01dff232e0a6d1f534954d5
adeolammy <adeolammy2020@gmail.com>
2023-02-21 15:58:35 +0100

    React has been setup

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+	return (
+		
+	<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+	);
+}
+
+export default App;
